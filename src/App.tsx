43080819fe69a0d208a4e8b9602b0134173a681f
Graeme MacFarlane <graeme@paddle.com>
2020-04-26 14:53:32 +0100

    setup vizualiser app to receive data-tray props

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,14 @@ import EventEmitter from "@trayio/builder-squad-event-emitter";
 function App() {
   return (
     <EventEmitter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <GraemesVisualizer />
     </EventEmitter>
   );
 }
+
+export const GraemesVisualizer = (props: any) => {
+  console.log(props);
+  return <div>graeme</div>;
+};
 
 export default App;
